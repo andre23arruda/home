5c2821b3df381128aaca8f4a371f84a3bdc59772
Andre <andre23arruda@gmail.com>
2021-12-05 17:23:39 -0400

    Updates

---
+++ precache-manifest.2d663e2c92420d2bc2f0b69b3151a4b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad1955a694f7ca8d3008770437cfcaab",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "d311aa444daec1239451",
+    "url": "/home/static/css/main.af6d9b4f.chunk.css"
+  },
+  {
+    "revision": "7d7a101ae03580d235aa",
+    "url": "/home/static/js/2.bd49d0ed.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/home/static/js/2.bd49d0ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d311aa444daec1239451",
+    "url": "/home/static/js/main.e8644fee.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "7dea8a01ecb7fe07debb9d3616fbaf87",
+    "url": "/home/static/media/andre.7dea8a01.png"
+  },
+  {
+    "revision": "ccb5346c6f2e3597d7ec9e4e0bb1610a",
+    "url": "/home/static/media/resume.ccb5346c.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.df84cd9571af5c9c3b4e97876ee49e51.js"
+  "/home/precache-manifest.2d663e2c92420d2bc2f0b69b3151a4b4.js"
 );
 
 self.addEventListener('message', (event) => {
